6a67cce5613d321f8cf98923e03f5d069f406d3f
loet6010 <loet6010@126.com>
2018-11-27 17:43:22 +0800

    sql比较原本

---
+++ src/main/java/com/study/thread/multithread/pay_route_pay_route_20181127_liuronghua_2 - 副本.sql
@@ -0,0 +1,41 @@
+--增加字段
+ALTER TABLE tbl_pay_channel ADD COLUMN share_status int2 DEFAULT 0 not NULL;
+ALTER TABLE tbl_pay_channel ADD COLUMN share_content VARCHAR;
+COMMENT ON COLUMN tbl_pay_channel.share_status is '共享标志：0，非共享渠道；1，共享渠道；默认0';
+COMMENT ON COLUMN tbl_pay_channel.share_content is '共享字段：是共享渠道且共享字段相同的多个渠道为共享渠道';
+
+
+--改原C1800369709渠道数据,测试环境id=10019, 现网id=10017
+update tbl_pay_channel
+set code = 'helibao3',share_status = 1, share_content = 'C1800372162'
+where id = 10019;
+
+--改新增C1800372162渠道数据,测试环境id=10026, 现网id=
+update tbl_pay_channel
+set share_status = 1, share_content = 'C1800372162'
+where id = 10026;
+
+--新增速借的switch表合利宝的支付渠道：测试环境channel_id=10026,现网channel_id=
+insert into tbl_pay_platform_switch(channel_id,platform,memo,onoff) values(10026,'sj','速借合利宝商保还款',0);
+
+--愿速借的连连还款渠道关闭:测试环境id=21, 现网id=15
+update tbl_pay_platform_switch set onoff = 1 where id = 21;
+
+
+--新增config表配置，测试环境channel_id=10026, 现网channel_id=
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10000,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10001,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10002,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10003,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10004,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10005,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10006,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10007,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10008,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10009,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10010,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10011,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10012,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10013,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10014,10026,100,1000000,10000000,10000000,0,'管理员');
+insert into tbl_pay_channel_config(bank_id,channel_id,fee_rate,per_limit,day_limit,month_limit,status,create_user) values(10015,10026,100,1000000,10000000,10000000,0,'管理员');
