7feabbd0d733b313a5ad13be707f4b3d54a83d36
罗锅 <liuronghua@hzshzx.com>
2021-06-22 17:46:27 +0800

    test commit

--- src/main/java/com/loet/mine/test/SimpleTest.java
+++ src/main/java/com/loet/mine/test/SimpleTest.java
@@ -1,8 +1,17 @@
 package com.loet.mine.test;
 
+import org.springframework.util.FileCopyUtils;
+import sun.misc.BASE64Encoder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.UUID;
 
 /**
  * @Author liurh
@@ -10,13 +19,58 @@ import java.util.Date;
  * @Date 2019/8/22
  */
 public class SimpleTest {
+    private static byte[] PDF_BYTES = null;
 
-    public static void main(String[] args) {
-        String year = "2000";
-        String month = "09";
-        String day = "01";
+    public static void main(String[] args) throws Exception {
+        String mmsSign = UUID.randomUUID().toString().replace("-", "").toLowerCase();
 
-        System.out.println(getAgeByBirth(year, month, day));
+        System.out.println(mmsSign);
+    }
+
+    private static String getPath(String path) {
+        if (!path.startsWith("http://") && !path.startsWith("https://")) {
+            path = "https://" + path;
+        }
+        return path;
+    }
+
+    private static String getPdfBytes(String templateUrl) throws Exception {
+        if (PDF_BYTES == null || PDF_BYTES.length <= 0) {
+            URL url = new URL(templateUrl);
+            InputStream inputStream = url.openConnection().getInputStream();
+            PDF_BYTES = FileCopyUtils.copyToByteArray(inputStream);
+        }
+
+        // 对字节数组Base64编码
+        BASE64Encoder encoder = new BASE64Encoder();
+        return encoder.encode(PDF_BYTES);
+        // return Base64Utils.encodeToString(PDF_BYTES);
+    }
+
+    public static String getFileBase64(String imgURL) {
+        ByteArrayOutputStream data = new ByteArrayOutputStream();
+        try {
+            // 创建URL
+            URL url = new URL(imgURL);
+            byte[] by = new byte[1024];
+            // 创建链接
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setConnectTimeout(5000);
+            InputStream is = conn.getInputStream();
+            // 将内容读取内存中
+            int len = -1;
+            while ((len = is.read(by)) != -1) {
+                data.write(by, 0, len);
+            }
+            // 关闭流
+            is.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // 对字节数组Base64编码
+        BASE64Encoder encoder = new BASE64Encoder();
+        return encoder.encode(data.toByteArray());
     }
 
 
@@ -38,10 +92,10 @@ public class SimpleTest {
                 age = 0;
             } else {
                 age = now.get(Calendar.YEAR) - birth.get(Calendar.YEAR);
-                if (now.get(Calendar.MONTH) < birth.get(Calendar.MONTH)){
+                if (now.get(Calendar.MONTH) < birth.get(Calendar.MONTH)) {
                     age = age - 1;
-                } else if (now.get(Calendar.MONTH) == birth.get(Calendar.MONTH)){
-                    if (now.get(Calendar.DAY_OF_MONTH) < birth.get(Calendar.DAY_OF_MONTH)){
+                } else if (now.get(Calendar.MONTH) == birth.get(Calendar.MONTH)) {
+                    if (now.get(Calendar.DAY_OF_MONTH) < birth.get(Calendar.DAY_OF_MONTH)) {
                         age = age - 1;
                     }
                 }
